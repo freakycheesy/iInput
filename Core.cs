c9f4b29eaf6983abfca07a6b716bf95fb9e1daf8
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-11-22 20:49:49 +0000

    link

--- Core.cs
+++ Core.cs
@@ -2,7 +2,7 @@
 global using Vector2 = UnityEngine.Vector2;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(iInput.Core), "iInput", "1.0.0", "freakycheesy", null)]
+[assembly: MelonInfo(typeof(iInput.Core), "iInput", "1.0.0", "freakycheesy", "https://github.com/freakycheesy/iInput.git")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 
 namespace iInput
